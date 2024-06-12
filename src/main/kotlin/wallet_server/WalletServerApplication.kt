2414773c0f0bd64ac701c61f59e749216a3f2afd
Tim Schauder <temscha@live.com>
2024-06-12 12:02:21 +0200

    Initialize wallet server

---
+++ src/main/kotlin/wallet_server/WalletServerApplication.kt
@@ -0,0 +1,11 @@
+package wallet_server
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WalletServerApplication
+
+fun main(args: Array<String>) {
+	runApplication<WalletServerApplication>(*args)
+}
