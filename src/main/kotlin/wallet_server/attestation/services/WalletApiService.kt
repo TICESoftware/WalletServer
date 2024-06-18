d31c91cfa2d90c392facbc507966028c3e26754e
Tim Schauder <temscha@live.com>
2024-06-18 13:30:37 +0200

    Generate nonces and save user

--- src/main/kotlin/wallet_server/attestation/services/WalletApiService.kt
+++ src/main/kotlin/wallet_server/attestation/services/WalletApiService.kt
@@ -1,15 +1,26 @@
 package wallet_server.attestation.services
 
 import org.springframework.stereotype.Service
+import wallet_server.attestation.repositories.UserEntity
+import wallet_server.attestation.repositories.UserRepository
 import wallet_server.attestation.requests.AttestationRequest
 import wallet_server.attestation.responses.AttestationResponse
 import wallet_server.attestation.responses.NonceResponse
 
 @Service
-class WalletApiService {
+class WalletApiService(private val userRepository: UserRepository) {
 
     fun requestNonces(walletInstanceId: String): NonceResponse {
-        val nonces = listOf("nonce1", "nonce2", "nonce3")
+        val nonces = List(2) { java.util.UUID.randomUUID().toString() }
+
+        val user = UserEntity(
+            walletInstanceId = walletInstanceId,
+            firstNonce = nonces[0],
+            secondNonce = nonces[1],
+            id = null
+        )
+
+        userRepository.save(user)
         return NonceResponse(nonces)
     }
 
