f31fa942bfaa99f157c14014f9efafaeabdf7326
Tim Schauder <temscha@live.com>
2024-06-18 20:26:36 +0200

    Sign incoming public key with private key for attestation

--- src/main/kotlin/wallet_server/attestation/services/WalletApiService.kt
+++ src/main/kotlin/wallet_server/attestation/services/WalletApiService.kt
@@ -1,14 +1,25 @@
 package wallet_server.attestation.services
 
+import io.github.cdimascio.dotenv.Dotenv
+import io.jsonwebtoken.Jwts
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import wallet_server.attestation.repositories.UserEntity
 import wallet_server.attestation.repositories.UserRepository
 import wallet_server.attestation.requests.AttestationRequest
 import wallet_server.attestation.responses.AttestationResponse
 import wallet_server.attestation.responses.NonceResponse
+import java.security.KeyFactory
+import java.security.spec.PKCS8EncodedKeySpec
+import java.util.*
 
 @Service
-class WalletApiService(private val userRepository: UserRepository) {
+class WalletApiService @Autowired constructor(
+    private val userRepository: UserRepository,
+
+) {
+    private val dotenv: Dotenv = Dotenv.load()
+    private val privateKey: String? = dotenv["PRIVATE_KEY"]
 
     fun requestNonces(walletInstanceId: String): NonceResponse {
         val nonces = List(2) { java.util.UUID.randomUUID().toString() }
@@ -25,6 +36,19 @@ class WalletApiService(private val userRepository: UserRepository) {
     }
 
     fun requestAttestation(requestAttestation: AttestationRequest): AttestationResponse {
-        return AttestationResponse(walletAttestation = "testWalletAttestation")
+        print( privateKey)
+
+        val pem = privateKey
+            ?.replace("-----BEGIN PRIVATE KEY-----", "")
+            ?.replace("-----END PRIVATE KEY-----", "")
+
+        val decodedKey = Base64.getDecoder().decode(pem)
+
+        val keySpec = PKCS8EncodedKeySpec(decodedKey)
+        val keyFactory = KeyFactory.getInstance("EC")
+        val privateKeyReloaded = keyFactory.generatePrivate(keySpec)
+
+        val walletAttestation: String = Jwts.builder().subject("Joe").signWith(privateKeyReloaded).compact()
+        return AttestationResponse(walletAttestation)
     }
 }
