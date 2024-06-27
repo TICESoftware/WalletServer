498a074b5fc644de371698d0ea5f2f7e14eaf003
Simon Kempendorf <s.kempendorf@tice-software.com>
2024-06-27 11:57:11 +0200

    Prepare linting

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     kotlin("plugin.spring") version "1.9.24"
     kotlin("plugin.noarg") version "2.0.0"
     kotlin("plugin.jpa") version "2.0.0"
+    id("com.diffplug.spotless") version "6.25.0"
 }
 
 group = "com.example"
@@ -22,29 +23,38 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation ("io.github.cdimascio:dotenv-java:2.2.0")
+    implementation("io.github.cdimascio:dotenv-java:2.2.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.5.0")
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation ("io.jsonwebtoken:jjwt-api:0.12.5")
-    implementation ("io.jsonwebtoken:jjwt-impl:0.12.5")
-    implementation ("io.jsonwebtoken:jjwt-jackson:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-api:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-impl:0.12.5")
+    implementation("io.jsonwebtoken:jjwt-jackson:0.12.5")
 
     developmentOnly("org.springframework.boot:spring-boot-devtools")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testImplementation ("org.mockito:mockito-core:3.+")
+    testImplementation("org.mockito:mockito-core:3.+")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
     runtimeOnly("com.h2database:h2")
 }
 
 kotlin {
-compilerOptions {
-    freeCompilerArgs.addAll("-Xjsr305=strict")
-}
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
 }
 
 tasks.withType<Test> {
-useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+spotless {
+    kotlin {
+//        ktlint()
+    }
+    kotlinGradle {
+        ktlint()
+    }
 }
